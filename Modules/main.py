2b2d9c8d184a9767848bc98181aca4adcbb337f0
Awesome Sauce <jmorenoalan@gmail.com>
2021-09-18 22:17:28 -0700

    Change

---
+++ Modules/main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkaddon import *
+root = Tk()
+#tka_entry = tka.entry("grid", root)
+#tka_entry.insert(0, "WHAT")
+#print(tka_entry.get())
+tka_button = tka.button("pack 0 1", root, "textclea")
+root.mainloop()
+
+
+
